23f57ed57b1f41e241a51c8a8f1547203c40c827
gejza <gejza@heisenberg>
2013-12-05 16:10:01 +0100

    voc 2013

--- kvocka.sh
+++ kvocka.sh
@@ -11,22 +11,24 @@ cat << END
  <head>
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
   <title>
-   VOC 2012
+   VOC 2013
   </title>
  </head>
 <body>
 END
 fi
 yacite read pubcit | 
-	yacite filter 'year==2012 and myown' > voc2012.myown.yaml
-yacite filter '"scopus" in tags' < voc2012.myown.yaml | 
+	yacite filter 'year==2013 and myown' > voc2013.myown.yaml
+yacite filter '"cc" in tags' < voc2013.myown.yaml | 
+	yacite render -e '{ title: "Karentované (Current Contents) časopisy zahraničné aj domáce" }' kvocka_moje.${SUFF}
+yacite filter '"scopus" in tags and "cc" not in tags' < voc2013.myown.yaml | 
 	yacite render -e '{ title: "Časopisy zahraničné aj domáce evidované v databáze SCOPUS" }' kvocka_moje.${SUFF}
 yacite read pubcit | 
-	yacite filter --notmyown 'year in [2011,2012] and "voc2011" not in tags' > voc2012.yaml
-yacite filter --notmyown '("wos" in tags or "sci" in tags or "scopus" in tags) and not "dautor" in tags' > voc2012.sci.no.dautor.yaml < voc2012.yaml
-yacite filter --notmyown '("wos" in tags or "sci" in tags or "scopus" in tags) and "dautor" in tags' > voc2012.sci.dautor.yaml < voc2012.yaml
-yacite render -e '{ title: "Citácie podľa SCI, multidisciplinárne ISI, SCOPUS – len zahraničný autor" }' kvocka.${SUFF} < voc2012.sci.no.dautor.yaml|tee voc2012.sci.no.dautor.${SUFF}
-yacite render -e '{ title: "Citácie podľa SCI, multidisciplinárne ISI, SCOPUS – domáci autor" }' kvocka.${SUFF} < voc2012.sci.dautor.yaml | tee voc2012.sci.dautor.${SUFF}
+	yacite filter --notmyown 'year in [2012,2013] and "voc2012" not in tags' > voc2013.yaml
+yacite filter --notmyown '("wos" in tags or "sci" in tags or "scopus" in tags) and not "dautor" in tags' > voc2013.sci.no.dautor.yaml < voc2013.yaml
+yacite filter --notmyown '("wos" in tags or "sci" in tags or "scopus" in tags) and "dautor" in tags' > voc2013.sci.dautor.yaml < voc2013.yaml
+yacite render -e '{ title: "Citácie podľa SCI, multidisciplinárne ISI, SCOPUS – len zahraničný autor" }' kvocka.${SUFF} < voc2013.sci.no.dautor.yaml|tee voc2013.sci.no.dautor.${SUFF}
+yacite render -e '{ title: "Citácie podľa SCI, multidisciplinárne ISI, SCOPUS – domáci autor" }' kvocka.${SUFF} < voc2013.sci.dautor.yaml | tee voc2013.sci.dautor.${SUFF}
 if test "$SUFF" == html; then
 cat <<END
 </body>

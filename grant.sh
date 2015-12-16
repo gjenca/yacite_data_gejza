bbce81a0085e427df784535ee5287faee337b198
gejza <gejza@alanko>
2015-12-16 12:22:35 +0100

    zav sprava

--- grant.sh
+++ grant.sh
@@ -13,10 +13,10 @@ cat << END
 <body>
 END
 yacite read pubcit | 
-	yacite filter 'year==2014 and myown and "'$GRANT'" in grants' | yacite sort -k year > grant.myown.yaml
+	yacite filter 'year>=2015 and myown and "'$GRANT'" in grants' | yacite sort -k year > grant.myown.yaml
 	yacite render -e '{title: Publikované články}' kvocka_moje.html < grant.myown.yaml
 yacite read pubcit | 
-	yacite filter --notmyown 'year == 2013' | 
+	yacite filter --notmyown 'year >= 2014' | 
 	yacite filter --myown '"'$GRANT'" in grants' |
 	yacite sort -k year > grant.yaml
 	yacite render -e '{title: Citácie}' kvocka.html < grant.yaml

b138cd554bfdbf89c14de878a3001340686d7e8d
gejza <gejza@heisenberg>
2018-12-07 13:22:59 +0100

    2018 clanok

--- grant.sh
+++ grant.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GRANT="$1"
+FROMYEAR=2015
 cat << END
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -7,16 +8,16 @@ cat << END
  <head>
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
   <title>
-   $GRANT za rok 2016
+   $GRANT za rok >=$FROMYEAR
   </title>
  </head>
 <body>
 END
 yacite read pubcit | 
-	yacite filter 'year>=2016 and myown and "'$GRANT'" in grants' | yacite sort -k year > grant.myown.yaml
+	yacite filter "year>=$FROMYEAR and myown and "'$GRANT'" in grants' | yacite sort -k year > grant.myown.yaml
 	yacite render -e '{title: Publikované články}' kvocka_moje.html < grant.myown.yaml
 yacite read pubcit | 
-	yacite filter --notmyown 'year >= 2016' | 
+	yacite filter --notmyown 'year >= 2015' | 
 	yacite filter --myown '"'$GRANT'" in grants' |
 	yacite sort -k year > grant.yaml
 	yacite render -e '{title: Citácie}' kvocka.html < grant.yaml
